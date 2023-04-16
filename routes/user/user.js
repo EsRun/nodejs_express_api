c1d9fc89c317dda82cd4f0f0fbf3bf1aef2e1944
비엔나 <sure@paran.com>
2023-04-16 21:28:25 +0900

    fix : route

--- routes/user/user.js
+++ routes/user/user.js
@@ -2,6 +2,14 @@ const express = require("express");
 const db = require("../database/dbQuery");
 const router = express.Router();
 
+// 아이디 확인
+router.get("/idCheck", function (req, res, next) {
+  const userinfo = req.query;
+  db.checkId(userinfo, (rows) => {
+    res.send(rows);
+  });
+});
+
 // 직원 정보
 router.get("/member", function (req, res, next) {
   const userinfo = req.query;
