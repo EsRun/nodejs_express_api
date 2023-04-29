ddcea3eee0c1ef68496934673d89d8c6087876bb
비엔나 <sure@paran.com>
2023-04-29 22:39:54 +0900

    add : comment

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -2,6 +2,8 @@ const express = require("express");
 const db = require("../../database/auth/authQuery");
 const router = express.Router();
 
+const token = null; // 로그인 토큰 처리 예정
+
 // 로그인
 router.get("/login", function (req, res, next) {
   const userinfo = req.query;
