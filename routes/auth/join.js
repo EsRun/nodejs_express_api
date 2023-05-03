ec0b950f87f6e2abcc9f3c9d0dace304d4c3182a
비엔나 <sure@paran.com>
2023-05-03 15:20:49 +0900

    add : join crypto test code

--- routes/auth/join.js
+++ routes/auth/join.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const db = require("../../database/auth/authQuery");
 const router = express.Router();
+import crypto from "crypto";
+
+const createHashedPassword = (password) => {
+  return crypto.createHash("sha512").update(password).digest("base64");
+};
 
 // 아이디 확인
 router.get("/idCheck", function (req, res, next) {
@@ -12,6 +17,7 @@ router.get("/idCheck", function (req, res, next) {
 
 // 회원가입
 router.get("/join", function (req, res, next) {
+  console.log(createHashedPassword("aa1234"));
   const userinfo = req.query;
   db.loginProc(userinfo, (rows) => {
     res.send(rows);
